ad911400a93189d7bcaebb899f7ebe6bd05bbf18
Richard Weiser <richard@weisertek.com>
2020-02-10 00:47:08 -0500

    Stage 2

---
+++ PayTell.ChessMoves.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using PayTel.ChessMoves.Library.Lib;
+
+namespace PayTel.ChessMoves.Console
+{
+    internal class Program
+    {
+        private static readonly Library.Lib.ChessMovesLib ChessMoves = new Library.Lib.ChessMovesLib();
+        
+
+        private static void Main()
+        {
+            while (1 == 1)
+            {
+                DoUserAction();
+            }
+        }
+
+
+        private static void DoUserAction()
+        {
+
+            System.Console.WriteLine("Please enter your selected piece [King, Queen, Rook, Bishop] and press enter: ");
+            var chessPiece = System.Console.ReadLine();
+            System.Console.WriteLine("Using standard chess notation, enter your starting point: ");
+            var startingCoordinate = System.Console.ReadLine();
+
+            var validMoves = ChessMoves.GetValidMoves(chessPiece, startingCoordinate);
+            System.Console.WriteLine(chessPiece + " - " + ChessMoves.GetChessMovesString(validMoves));
+        }
+    }
+}
